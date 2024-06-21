42fa6a7ffc3d43c505383548344f12859a3288e8
Muskan <vermamuskan181@gmail.com>
2024-06-21 13:01:18 +0530

    first version of our hotels

--- newExpress.js
+++ newExpress.js
@@ -13,7 +13,7 @@ const PersonRoute = require('./routes/PersonRoute')
 const MenuRoute = require('./routes/MenuRoute')
 //use the router
 app.use('/person', PersonRoute)
-app.use('/menu', MenuRoute);
+app.use('/menu', MenuRoute)
 const port = 3000
 app.listen(port, () => {
   console.log(`Server is listening ${port}`)
