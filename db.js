0e5ca289fe0c0ccd8e25f39b5e18215eea6aa2df
Muskan <vermamuskan181@gmail.com>
2024-06-22 18:18:45 +0530

    Modify the code

--- db.js
+++ db.js
@@ -1,22 +1,27 @@
-const mongoose=require('mongoose')
-require('dotenv').config();
-//define the mongodb connection url
-const mongodbUrl=process.env.DB_URL;
+const mongoose = require('mongoose')
+require('dotenv').config()
 
+// Define the MongoDB connection URL
+const mongodbUrl = process.env.DB_URL
 
-//setUp mongoose
-mongoose.connect(mongodbUrl,{
-  useNewURLparser:true,//required parameters
-  useUnifiedTopology:true
+// Set up mongoose
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true, // corrected typo here
+  useUnifiedTopology: true
 })
-const db=mongoose.connection;
-db.on('connected',()=>{
+
+const db = mongoose.connection
+
+db.on('connected', () => {
   console.log('Connected to MongoDB server')
 })
-db.on('disconnected',()=>{
-  console.log('disConnected to MongoDB server')
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB server')
 })
-db.on('error',(err)=>{
-  console.error('Error to MongoDB server',err)
+
+db.on('error', err => {
+  console.error('Error connecting to MongoDB server', err)
 })
-module.exports=db;
+
+module.exports = db
